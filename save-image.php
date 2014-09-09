95e82346fa514a3b50023d164f5b000ecefe871b
Gilles Vandenoostende <gilles.vandenoostende@duvalguillaume.com>
2014-09-09 11:24:07 +0200

    working file save

--- save-image.php
+++ save-image.php
@@ -1,5 +1,4 @@
 <?php
-	
 	// write generated image to server
 	$guid = $_POST['guid'];
 	$image = $_POST['image'];
@@ -14,21 +13,4 @@
 	$decodedData = base64_decode($encodedData);
 	
 	file_put_contents($uploadfile, $decodedData);
-	
-	/*
-	$uploaddir = 'uploads';
-	$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
-	
-	echo '<pre>';
-	if (move_uploaded_file($_FILES['file']['tmp_name'], $uploadfile)) {
-	    echo "File is valid, and was successfully uploaded.\n";
-	} else {
-	    echo "Possible file upload attack!\n";
-	}
-	
-	echo 'Here is some more debugging info:';
-	print_r($_FILES);
-	
-	print "</pre>";
-	*/
 ?>
